61d192ceb075ba0b164f106e19a9a6f4889b8b19
anniebuue <30415792+anniebuue@users.noreply.github.com>
2026-03-26 12:02:52 -0500

    InterpolatedCamera2D: Fix functionality, add group

--- scripts/rubicon_interpolated_camera_2d.gd
+++ scripts/rubicon_interpolated_camera_2d.gd
@@ -1,19 +1,22 @@
 @tool
 class_name RubiconInterpolatedCamera2D extends Camera2D
 
+@export_group("Position", "position_")
 @export var position_interpolate : bool = true
 @export var position_interpolate_target : Vector2 = Vector2.ZERO
 @export var position_interpolate_offset : Vector2 = Vector2.ZERO
 @export var position_interpolate_speed : float = 2.4 ## How fast the camera's position will return to, in px/s.
 
+@export_group("Rotation", "rotation_")
 @export var rotation_interpolate : bool = true
 @export var rotation_interpolate_target : float = 0.0 ## The camera's target rotation, in radians.
 @export var rotation_interpolate_offset : float = 0.0
 @export var rotation_interpolate_speed : float = 2.4 ## How fast the camera's rotation will return to, in px/s.
 
+@export_group("Zoom", "zoom_")
 @export var zoom_interpolate : bool = true
 @export var zoom_interpolate_target : Vector2 = Vector2.ONE
-@export var zoom_interpolate_offset : Vector2 = Vector2.ONE
+@export var zoom_interpolate_offset : Vector2 = Vector2.ZERO
 @export var zoom_interpolate_speed : float = 3.125 ## How fast the camera's zoom will return to, in px/s.
 
 func _ready() -> void:
@@ -21,12 +24,12 @@ func _ready() -> void:
 	rotation_interpolate_target = global_rotation
 	zoom_interpolate_target = zoom
 
-func _process(_delta : float) -> void:
+func _process(delta : float) -> void:
 	if position_interpolate:
-		global_position = global_position.lerp(position_interpolate_target + position_interpolate_offset, position_interpolate_speed)
+		global_position = global_position.lerp(position_interpolate_target + position_interpolate_offset, position_interpolate_speed * delta)
 	
 	if rotation_interpolate:
-		global_rotation = lerpf(global_rotation, rotation_interpolate_target + rotation_interpolate_offset, rotation_interpolate_speed)
+		global_rotation = lerpf(global_rotation, rotation_interpolate_target + rotation_interpolate_offset, rotation_interpolate_speed * delta)
 	
 	if zoom_interpolate:
-		zoom = zoom.lerp(zoom_interpolate_target + zoom_interpolate_offset, zoom_interpolate_speed)
+		zoom = zoom.lerp(zoom_interpolate_target + zoom_interpolate_offset, zoom_interpolate_speed * delta)

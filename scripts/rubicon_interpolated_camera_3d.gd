a22bf767fcdff234082f855f86125340b9a514c3
anniebuue <30415792+anniebuue@users.noreply.github.com>
2026-03-26 11:56:42 -0500

    InterpolatedCamera3D: Replace rotation with basis

--- scripts/rubicon_interpolated_camera_3d.gd
+++ scripts/rubicon_interpolated_camera_3d.gd
@@ -4,29 +4,29 @@ class_name RubiconInterpolatedCamera3D extends Camera3D
 @export var position_interpolate : bool = true
 @export var position_interpolate_target : Vector3 = Vector3.ZERO
 @export var position_interpolate_offset : Vector3 = Vector3.ZERO
-@export var position_interpolate_speed : float = 2.4 ## How fast the camera's position will return to, in px/s.
+@export var position_interpolate_speed : float = 2.4 ## How fast the camera's position will return to. 
 
-@export var rotation_interpolate : bool = true
-@export var rotation_interpolate_target : Vector3 = Vector3.ZERO ## The camera's target rotation, in radians.
-@export var rotation_interpolate_offset : Vector3 = Vector3.ZERO
-@export var rotation_interpolate_speed : float = 2.4 ## How fast the camera's rotation will return to, in px/s.
+@export var basis_interpolate : bool = true
+@export var basis_interpolate_target : Basis = Basis.IDENTITY ## The camera's target basis.
+@export var basis_interpolate_offset : Basis = Basis.IDENTITY
+@export var basis_interpolate_speed : float = 2.4 ## How fast the camera's basis will return to.
 
 @export var fov_interpolate : bool = true
 @export var fov_interpolate_target : float = 45.0
 @export var fov_interpolate_offset : float = 45.0
-@export var fov_interpolate_speed : float = 3.125 ## How fast the camera's FOV will return to, in px/s.
+@export var fov_interpolate_speed : float = 3.125 ## How fast the camera's FOV will return to.
 
 func _ready() -> void:
 	position_interpolate_target = global_position
-	rotation_interpolate_target = global_rotation
+	basis_interpolate_target = global_basis
 	fov_interpolate_target = fov
 
 func _process(delta : float) -> void:
 	if position_interpolate:
 		global_position = global_position.lerp(position_interpolate_target + position_interpolate_offset, position_interpolate_speed * delta)
 	
-	if rotation_interpolate:
-		global_rotation = global_rotation.lerp(rotation_interpolate_target + rotation_interpolate_offset, rotation_interpolate_speed * delta)
+	if basis_interpolate:
+		global_basis = global_basis.slerp(basis_interpolate_target * basis_interpolate_offset, basis_interpolate_speed * delta)
 	
 	if fov_interpolate:
 		fov = lerpf(fov, fov_interpolate_target + fov_interpolate_offset, fov_interpolate_speed * delta)

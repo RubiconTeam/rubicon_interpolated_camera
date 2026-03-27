65f07d468c7a7e8fbd81d9f7c4118821ef5dcd70
legole0 <legole0@proton.me>
2026-03-26 23:38:11 -0300

    InterpolatedCamera2D: Make enabled properties into bool groups

--- scripts/rubicon_interpolated_camera_2d.gd
+++ scripts/rubicon_interpolated_camera_2d.gd
@@ -2,19 +2,19 @@
 class_name RubiconInterpolatedCamera2D extends Camera2D
 
 @export_group("Position", "position_interpolate_")
-@export var position_interpolate_enabled: bool = true
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var position_interpolate_enabled: bool = true
 @export var position_interpolate_target: Vector2 = Vector2.ZERO
 @export var position_interpolate_offset: Vector2 = Vector2.ZERO
 @export var position_interpolate_speed: float = 2.4 ## How fast the camera's position will return to, in px/s.
 
 @export_group("Rotation", "rotation_interpolate_")
-@export var rotation_interpolate_enabled: bool = true
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var rotation_interpolate_enabled: bool = true
 @export var rotation_interpolate_target: float = 0.0 ## The camera's target rotation, in radians.
 @export var rotation_interpolate_offset: float = 0.0
 @export var rotation_interpolate_speed: float = 2.4 ## How fast the camera's rotation will return to, in px/s.
 
 @export_group("Zoom", "zoom_interpolate_")
-@export var zoom_interpolate_enabled: bool = true
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var zoom_interpolate_enabled: bool = true
 @export_custom(PROPERTY_HINT_LINK, "") var zoom_interpolate_target : Vector2 = Vector2.ONE
 @export_custom(PROPERTY_HINT_LINK, "") var zoom_interpolate_offset : Vector2 = Vector2.ZERO
 @export var zoom_interpolate_speed: float = 3.125 ## How fast the camera's zoom will return to, in px/s.
